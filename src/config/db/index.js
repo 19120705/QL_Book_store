dddb70f5edde60597ebfcbaba14bf62a65c5abd9
Holmes32 <quoctuan323@gmail.com>
2022-06-13 22:49:36 +0700

    delete mongoose

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 //const initModels = require("../../app/models/init-models");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST, port: process.env.DB_PORT, 
-    dialect: 'mysql',
+    dialect: 'mysql'
     // dialectOptions: {
     //     socketPath: process.env.DB_HOST
     // },
@@ -14,6 +14,6 @@ sequelize.authenticate().then(() => {
     console.log("NotConnected"); 
 })
 module.exports = {
-    sequelize,
+    sequelize
     //models: initModels(sequelize),
 };
