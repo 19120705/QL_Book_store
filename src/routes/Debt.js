e98fbce3cf9149a28aa05ff4133d90f85a1e6a49
Holmes32 <quoctuan323@gmail.com>
2022-06-24 17:18:05 +0700

    final of final

--- src/routes/Debt.js
+++ src/routes/Debt.js
@@ -4,10 +4,4 @@ const debtController = require('../app/controllers/debtController');
 //const upload = require('../app/middlewares/uploadIMG/multer')
 
 router.get('/',debtController.list)
-router.get('/print',debtController.printMonth)
-router.get('/info/:id',debtController.info)
-router.post('/:id/add',debtController.add)
-router.post('/:id/pay',debtController.pay)
-router.get('/hisAdd',debtController.hisAdd)
-router.get('/hisPay',debtController.hisPay)
 module.exports =router;
